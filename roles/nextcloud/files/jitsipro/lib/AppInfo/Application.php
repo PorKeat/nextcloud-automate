49007ad4b4a4e04ae10d40fa2aff35385413da19
PorKeat <alexkgm2412@gmail.com>
2026-08-13 18:17:06 +0700

    fix(jitsipro): completely remove legacy navigation PHP code and use modern info.xml navigation

--- roles/nextcloud/files/jitsipro/lib/AppInfo/Application.php
+++ roles/nextcloud/files/jitsipro/lib/AppInfo/Application.php
@@ -16,25 +16,5 @@ class Application extends App implements IBootstrap {
     }
 
     public function boot(IBootContext $context): void {
-        $context->injectFn(\Closure::fromCallable([$this, 'registerNavigation']));
-    }
-
-    protected function registerNavigation(\OCP\Navigation\IManager $navigationManager, \OCP\IURLGenerator $urlGenerator, \OCP\IConfig $config): void {
-        $appName = $config->getAppValue('jitsipro', 'app_name', 'Video Call');
-        $appIcon = $config->getAppValue('jitsipro', 'app_icon', '');
-        
-        if (empty($appIcon)) {
-            $appIcon = $urlGenerator->imagePath('jitsipro', 'app.svg');
-        }
-
-        $navigationManager->add(function() use ($urlGenerator, $appName, $appIcon) {
-            return [
-                'id' => 'jitsipro',
-                'order' => 10,
-                'href' => $urlGenerator->linkToRoute('jitsipro.page.index'),
-                'icon' => $appIcon,
-                'name' => $appName,
-            ];
-        });
     }
 }
