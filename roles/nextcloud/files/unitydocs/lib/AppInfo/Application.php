e31d0a447e283d1c5ac9e82b5f3d35611a1701a2
PorKeat <alexkgm2412@gmail.com>
2026-08-13 16:59:39 +0700

    fix(unityworkspace): completely remove legacy navigation PHP code and use modern info.xml navigation

--- roles/nextcloud/files/unitydocs/lib/AppInfo/Application.php
+++ roles/nextcloud/files/unitydocs/lib/AppInfo/Application.php
@@ -14,22 +14,5 @@ class Application extends App implements IBootstrap {
     }
 
     public function boot(IBootContext $context): void {
-        $this->registerNavigation();
-    }
-
-    protected function registerNavigation(): void {
-        $server = \OC::$server;
-        $navigationManager = $server->getNavigationManager();
-        $urlGenerator = $server->getURLGenerator();
-        
-        $navigationManager->add(function() use ($urlGenerator) {
-            return [
-                'id' => 'unitydocs',
-                'order' => 2,
-                'href' => $urlGenerator->linkToRoute('unitydocs.page.index'),
-                'icon' => $urlGenerator->imagePath('core', 'filetypes/text.svg'),
-                'name' => 'Docs',
-            ];
-        });
     }
 }
